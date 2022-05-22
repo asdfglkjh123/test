a00b26fcbab24b1d845d7d36b1cab8dab2bb7c0f
Salva <asdfglkjh123@protonmail.com>
2022-05-23 00:49:06 +0300

    commit

--- src/views/utilities/Shadow.js
+++ src/views/utilities/Shadow.js
@@ -40,9 +40,9 @@ const UtilitiesShadow = () => {
             formatter: '{a} <br/>{b} : {c} ({d}%)'
         },
         legend: {
-            orient: 'vertical',
-            left: 'left',
-            data: ['Total Staked', 'PancakeSwap Pool', 'Circulating Supply (excl. PCS)', 'Locked for sSTX holders'],
+            orient: 'horizontal',
+            left: 'center',
+            data: ['Locked for sSTX holders', 'PancakeSwap Pool', 'Circulating Supply (excl. PCS)', 'Total Staked'],
             textStyle: {
                 color: theme.palette.success.main
             }
@@ -52,54 +52,59 @@ const UtilitiesShadow = () => {
                 name: 'Supply & Staking Stats',
                 type: 'pie',
                 radius: '55%',
-                center: ['50%', '70%'],
+                center: ['50%', '60%'],
                 label: {
                     normal: {
                         textStyle: {
-                            color: theme.palette.success.main
+                            color: theme.palette.grey[50]
                         }
                     }
                 },
                 data: [
-                    { value: 37355902, name: 'Total Staked' },
+                    { value: 37355902, name: 'Locked for sSTX holders' },
                     { value: 9020992, name: 'PancakeSwap Pool' },
                     { value: 48002553, name: 'Circulating Supply (excl. PCS)' },
-                    { value: 7000000, name: 'Locked for sSTX holders' }
+                    { value: 7000000, name: 'Total Staked' }
                 ]
             }
         ]
     };
     return (
-        <Grid container sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Grid container xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
             <Typography textAlign="center" variant="h2">
                 STAX Global Stats
             </Typography>
             <QueryStatsIcon sx={{ ml: 1 }} fontSize="medium" color="success" />
-            <Grid container sx={{ mt: 3, display: 'flex', justifyContent: 'center' }}>
-                <Button size="small" color="success" variant="outlined">
+            <Grid container xs={12} sx={{ mt: 3, display: 'flex', justifyContent: 'center' }}>
+                <Button size="large" color="success" variant="outlined">
                     <LayersIcon color="success" />
-                    Staking
                 </Button>
-                <Button size="small" color="success" sx={{ ml: 1 }} variant="outlined">
+                <Button size="large" color="success" sx={{ ml: 2 }} variant="outlined">
                     <CandlestickChartIcon color="success" />
-                    Market
                 </Button>
-                <Button size="small" color="success" sx={{ ml: 1 }} variant="outlined">
+                <Button size="large" color="success" sx={{ ml: 2 }} variant="outlined">
                     <PriceChangeIcon color="success" />
-                    Shares
                 </Button>
-                <Button size="small" color="success" sx={{ ml: 1 }} variant="outlined">
+                <Button size="large" color="success" sx={{ ml: 2 }} variant="outlined">
                     <SwitchAccountIcon color="success" />
-                    My Stats
                 </Button>
             </Grid>
-            <Grid item container sx={{ display: 'flex', justifyContent: 'center', mt: 10 }}>
-                <Grid item lg={12}>
-                    <Typography variant="h3" sx={{ mb: 5, color: theme.palette.grey[50], textAlign: 'center' }}>
-                        Staking Stats
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} lg={6} sx={{ mb: 20 }}>
+            <Grid
+                item
+                container
+                lg={7}
+                sx={{
+                    borderLeft: 3,
+                    borderBottom: 3,
+                    borderRadius: 5,
+                    borderColor: theme.palette.success.main,
+                    mt: 5,
+                    mb: 10,
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}
+            >
+                <Grid item xs={12} lg={12} sx={{ mb: 10, mt: 5 }}>
                     <EChartsReact option={option} style={{ height: 400 }} />
                 </Grid>
             </Grid>
